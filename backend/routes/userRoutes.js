aee85048ccba3225cd43275ec4384024d6dc4ccb
Personal <garrett.hamelin@gmail.com>
2023-08-30 15:55:18 -0600

    add rest of user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,14 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userController.js';
+import {  login, auth,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile} from '../controllers/userController.js';
 
-router.post('/auth', authUser);
+router.post('/login', login);
+router.post('/auth', auth);
+router.post('/logout', logoutUser);
+router.get('/profile', getUserProfile);
+router.put('/profile', updateUserProfile);
 
 export default router;
