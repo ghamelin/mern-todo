9c39e61883dd8d5957a742fb3e3e7c709a968ede
Personal <garrett.hamelin@gmail.com>
2023-09-01 14:21:07 -0600

    Add Profile and update for user

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,32 +1,42 @@
 import { apiSlice } from './apiSlice';
 
-const USERS_URL = 'api/users'
-
+const USERS_URL = 'api/users';
 
 export const usersApiSlice = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({
-    login: builder.mutation({
-      query: (credentials) => ({ 
-        url: `${USERS_URL}/auth`,
-        method: 'POST',
-        body: credentials
-      })
-    }),
-    register: builder.mutation({
-      query: (credentials) => ({ 
-        url: `${USERS_URL}`,
-        method: 'POST',
-        body: credentials
-      })
-    }),
-    logout: builder.mutation({
-      query: () => ({
-        url: `${USERS_URL}/logout`,
-        method: 'POST',
-      })
-    }),
-  })
+	endpoints: (builder) => ({
+		login: builder.mutation({
+			query: (credentials) => ({
+				url: `${USERS_URL}/auth`,
+				method: 'POST',
+				body: credentials,
+			}),
+		}),
+		register: builder.mutation({
+			query: (credentials) => ({
+				url: `${USERS_URL}`,
+				method: 'POST',
+				body: credentials,
+			}),
+		}),
+		logout: builder.mutation({
+			query: () => ({
+				url: `${USERS_URL}/logout`,
+				method: 'POST',
+			}),
+		}),
+		updateUser: builder.mutation({
+			query: (credentials) => ({
+				url: `${USERS_URL}/profile`,
+				method: 'PUT',
+				body: credentials,
+			}),
+		}),
+	}),
 });
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation } = usersApiSlice;
-
+export const {
+	useLoginMutation,
+	useLogoutMutation,
+	useRegisterMutation,
+	useUpdateUserMutation,
+} = usersApiSlice;
