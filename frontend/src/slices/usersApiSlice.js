cf6533cfc41e335f8317d2e5a36067fcc399a4e0
Personal <garrett.hamelin@gmail.com>
2023-08-31 19:19:08 -0600

    add redux toolkit and set up api connection

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,19 @@
+import { apiSlice } from './apiSlice';
+
+const USERS_URL = 'api/users'
+
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({ 
+        url: `${USERS_URL}/auth`,
+        method: 'POST',
+        body: credentials
+      })
+    }),
+  })
+});
+
+export const { useLoginMutation } = usersApiSlice;
+
