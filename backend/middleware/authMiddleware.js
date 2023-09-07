9bdd340d5be3699a6b0ad92b2b260a7a79f31481
Personal <garrett.hamelin@gmail.com>
2023-09-06 20:09:31 -0600

    remove console log

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,4 +1,3 @@
-import cookieParser, { JSONCookie } from "cookie-parser";
 import AsyncHandler from "express-async-handler";
 import client from "../config/stytch.js";
 
@@ -7,7 +6,6 @@ const protect = AsyncHandler(async (req, res, next) => {
   const sessionToken = JSON.parse(req.cookies.sessionToken)?.session_token;
   client.sessions.authenticate({session_token: sessionToken}).then((response) => {
     req.user = response.user;
-    console.log('req.user:', req.user);
     next();
   }).catch((error) => {
   console.log(error);
